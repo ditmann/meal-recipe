6347e1db4d158dec71c05a5b461d824922d3eea5
Adrian <adriandenutrolige@gmail.com>
2025-12-17 14:02:28 +0100

    Made service class

---
+++ core/Service/calculateValueOfRecipe.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Adrian on 12/17/2025.
+//
+
+#include "calculateValueOfRecipe.h"
---
+++ core/Service/calculateValueOfRecipe.h
@@ -0,0 +1,14 @@
+//
+// Created by Adrian on 12/17/2025.
+//
+
+#ifndef MEAL_RECIPE_CALCULATEVALUEOFRECIPE_H
+#define MEAL_RECIPE_CALCULATEVALUEOFRECIPE_H
+
+
+class calculateValueOfRecipe
+{
+};
+
+
+#endif //MEAL_RECIPE_CALCULATEVALUEOFRECIPE_H
