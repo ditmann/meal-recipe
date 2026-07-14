dc6661e4786c2d08a21c59cb9d9d54ad23fc0e57
Adrian Ditmansen <45669062+ditmann@users.noreply.github.com>
2026-07-14 10:55:03 +0200

    Update and rename File_manager.rs to file_manager.rs
    ty Bendik

--- src/file_manager.rs
+++ src/file_manager.rs
@@ -11,7 +11,7 @@ pub struct File {
 //her returnerer vi et result som kan være at noe gikk galt i ?
 // men hvis ikke skal resultatet være en slik vektor som er av typen pathbuf
 pub fn read_files() -> io::Result<Vec<Recipe>>{
-    let storage :&Path = Path::new(r"C:\koding\meal-recipe\recipes");
+    let storage :&Path = Path::new(r"recipes");
     let mut jsons = fs::read_dir(storage)?
         .map(|infolder|infolder.map(|json|json.path()))
         .collect::<Result<Vec<_> , io::Error>>()?;
